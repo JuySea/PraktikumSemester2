46334d8b6b00dda30740a3fe2e8be6f6909994c8
Jaisy M. F <91929569+JuySea@users.noreply.github.com>
2022-04-23 23:13:38 +0800

    Update pasien.php

--- project_web1/pasien.php
+++ project_web1/pasien.php
@@ -3,14 +3,16 @@
 class Pasien {
     public $nama;
     public $tgl_periksa;
+    public $kode;
     public $tmp_lahir;
     public $tgl_lahir;
     public $email;
     public $gender;
     
-    function __construct($tgl_periksa, $nama, $tmp_lahir, $tgl_lahir, $gender, $email){
+    function __construct($tgl_periksa, $kode, $nama, $tmp_lahir, $tgl_lahir, $gender, $email){
         $this->tgl_periksa = $tgl_periksa;
         $this->nama = $nama;
+        $this->kode = $kode;
         $this->tmp_lahir = $tmp_lahir;
         $this->tgl_lahir = $tgl_lahir;
         $this->gender = $gender;
